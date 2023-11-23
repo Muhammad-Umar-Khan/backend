2073563bd6e0d4437ee667ebe4032253cfcf2239
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2023-11-23 19:45:52 +0500

    async

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
-const textIn = fs.readFileSync("./input.txt", "utf-8");
-console.log(textIn);
 const textIn02 = fs.readFile("./input.tsx", () =>
   console.log("Read file Async")
 );
+const textIn = fs.readFileSync("./input.txt", "utf-8");
 console.log(textIn02);
+console.log(textIn);
