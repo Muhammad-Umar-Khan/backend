a757443474a96316e6270257a649adbf46be2f0e
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2023-11-24 18:35:58 +0500

    comments

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// const fs = require("fs");
-// const textIn02 = fs.readFile("./input.tsx", () =>
-//   console.log("Read file Async")
-// );
-// const textIn = fs.readFileSync("./input.txt", "utf-8");
-// console.log(textIn02);
-// console.log(textIn);
-// console.log("Consistency!");
+const fs = require("fs");
+const textIn02 = fs.readFile("./input.tsx", () =>
+  console.log("Read file Async")
+);
+const textIn = fs.readFileSync("./input.txt", "utf-8");
+console.log(textIn02);
+console.log(textIn);
+console.log("Consistency!");
 const http = require("http"); //this module gives you networking capabilities;
