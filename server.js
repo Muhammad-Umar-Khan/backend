16333058ac767fb27afc6a19127b08376acc0273
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2023-11-27 18:17:33 +0500

    port 3003

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const http = require("http"); //this module gives you networking capabilities;
-http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
   res.end("This is my first server!!");
+});
+
+server.listen(3001, () => {
+  console.log("Listening on port 3000");
 });
