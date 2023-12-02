118f80d8950d420b0df5ba304486a2ec349cb3e5
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2023-12-02 23:14:19 +0500

    template overview

--- server.js
+++ server.js
@@ -8,10 +8,19 @@ const tempCard = fs.readFileSync(
   `${__dirname}/templates/template-card.html`,
   "utf-8"
 );
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
 const jsonData = fs.readFileSync("./dev-data/data.json", "utf-8");
 const server = http.createServer((req, res) => {
   const pathName = req.url;
-  if (pathName === "/" || pathName === "/product") {
+  if (pathName === "/" || pathName === "/overview") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+    res.end(tempOverview);
+  } else if (pathName === "/product") {
     res.end("Product page");
   } else if (pathName === "/service") {
     res.end("Service page");
